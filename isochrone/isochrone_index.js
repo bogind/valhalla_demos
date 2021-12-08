af0a75f858a8ae9faa49954a721c73bc4a840e0e
Dror Bogin <Dror.Bogin@gmail.com>
2021-12-08 10:07:10 +0200

    added attribuiton to isochrone

--- isochrone/isochrone_index.js
+++ isochrone/isochrone_index.js
@@ -4,7 +4,8 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 });
 
 var map = L.map('map',{
-    layers: [OpenStreetMap_Mapnik]
+    layers: [OpenStreetMap_Mapnik],
+
 }).setView([32.055572,34.756429], 13);
 
 const initLocation = {lat: 32.055572, lng: 34.756429};
@@ -16,5 +17,7 @@ map.addLayer(marker);
 marker.on('dragend',getContours)
 
 L.control.inputs({ position: 'topright' }).addTo(map);
+map.attributionControl.addAttribution('Server hosted by <a href="https://gis-ops.com">gis-ops</a> and funded by <a href="https://www.fossgis.de/">FOSSGIS e.V</a>')
+
 
 map.on('click', onMapClick);
